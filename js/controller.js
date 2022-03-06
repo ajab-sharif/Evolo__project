baf51982bcd5012f4015fe907043747f3754243d
sharif <sharif@ajab.sharifulislam.com>
2022-03-06 07:11:23 +0300

    add new components

--- js/controller.js
+++ js/controller.js
@@ -15,3 +15,37 @@ document.querySelector('.nav__mobile').addEventListener('click', function () {
     navMobileList.classList.toggle('nav--close')
     navMobileList.classList.toggle('nav--open')
 });
+
+
+$(document).ready(function () {
+    $('.customer-logos').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: true,
+        responsive: [{
+            breakpoint: 1000,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 800,
+            settings: {
+                slidesToShow: 3
+            }
+        }, {
+            breakpoint: 530,
+            settings: {
+                slidesToShow: 2
+            }
+        }, {
+            breakpoint: 430,
+            settings: {
+                slidesToShow: 1
+            }
+        }]
+    });
+});
